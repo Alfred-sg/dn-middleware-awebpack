581d52b961ee2474b1e47953fbce31b0af5f24a6
Alfred Qiu <sc941203@gmail.com>
2018-10-16 23:44:16 +0800

    modify

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './assets/common.less';
 import App from './components/App';
 
+console.log(App)
+
 ReactDOM.render(<App />, document.getElementById('root'));
